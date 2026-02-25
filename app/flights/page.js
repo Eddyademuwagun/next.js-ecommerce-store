5ecc392857637f98fbefd8cff222e558075ef2b1
Eddyademuwagun <ademuwaguneddy@gmail.com>
2026-02-25 14:26:46 +0100

    Modify flights

--- app/flights/page.js
+++ app/flights/page.js
@@ -1,3 +1,12 @@
+import styles from './Flights.module.scss';
+
 export default function FlightsPage() {
-  return <div>flights</div>;
+  return (
+    <div className={styles.flights}>
+      flights
+      <br></br>
+      <br></br>
+      Discover your next adventure
+    </div>
+  );
 }
