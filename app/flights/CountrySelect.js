bd7da11df514cf31914c83c20cad3294b7ce0424
Eddyademuwagun <ademuwaguneddy@gmail.com>
2026-02-27 01:13:43 +0100

    Add date and destination

---
+++ app/flights/CountrySelect.js
@@ -0,0 +1,6 @@
+export default function CountrySelect() {
+  return;
+  <>
+    <select onChange={(e) => setHue(e.target.value)}></select>
+  </>;
+}
--- app/flights/page.js
+++ app/flights/page.js
@@ -1,12 +1,84 @@
+'use client';
+
+import { useState } from 'react';
 import styles from './Flights.module.scss';
 
 export default function FlightsPage() {
+  const [selectedDepartureDate, setSelectedDepartureDate] = useState('');
+  const [selectedReturnDate, setSelectedReturnDate] = useState('');
+  const [selectFrom, setSelectFrom] = useState('');
+  const [selectTo, setSelectTo] = useState('');
   return (
-    <div className={styles.flights}>
-      flights
-      <br></br>
-      <br></br>
-      Discover your next adventure
-    </div>
+    <>
+      <div className={styles.flights}>
+        <h1>flights</h1>
+        <br></br>
+        <br></br>
+        <p>Discover your next adventure</p>
+      </div>
+
+      <div className={styles.fligtContainer}>
+        <div className={styles.flightCard}>
+          <div className={styles.fligtCardContainer}>
+            <div>From</div>
+            <select
+              value={selectFrom}
+              onChange={(e) => setSelectFrom(e.target.value)}
+            >
+              <option value="">Choose city</option>
+              <option value="vienna">Vienna</option>
+              <option value="amsterdam">Amsterdam</option>
+              <option value="london">London</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.flightCard}>
+          <div className={styles.fligtCardContainer}>
+            <div>To</div>
+            <select
+              value={selectTo}
+              onChange={(e) => setSelectTo(e.target.value)}
+            >
+              <option value="">Choose city</option>
+              <option value="vienna">Vienna</option>
+              <option value="amsterdam">Amsterdam</option>
+              <option value="london">London</option>
+            </select>
+          </div>
+        </div>
+        <div className={styles.flightCard}>
+          <div className={styles.fligtCardContainer}>
+            <div>Departure</div>
+            <div className={styles.dateTab}>
+              <input
+                id="flight-date"
+                type="date"
+                value={selectedDepartureDate}
+                onChange={(e) => setSelectedDepartureDate(e.target.value)}
+              />
+            </div>
+            {selectedDepartureDate && (
+              <p className={styles.selectedDate}>{selectedDepartureDate}</p>
+            )}
+          </div>
+        </div>
+        <div className={styles.flightCard}>
+          <div className={styles.fligtCardContainer}>
+            <div>Return</div>
+            <div className={styles.dateTab}>
+              <input
+                id="flight-date"
+                type="date"
+                value={selectedReturnDate}
+                onChange={(e) => setSelectedReturnDate(e.target.value)}
+              />
+            </div>
+            {selectedReturnDate && (
+              <p className={styles.selectedDate}>{selectedReturnDate}</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
